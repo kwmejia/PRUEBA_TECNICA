b6c22c5ade70ca01e2293423c72ad4f2a52514f8
harlinsonvasquez <harvack25@gmail.com>
2024-09-20 13:52:13 -0500

    create folder proyect

--- BackendSpringboot/src/main/java/RIwi/BackendSpringboot/model/entities/CorralEntity.java
+++ BackendSpringboot/src/main/java/RIwi/BackendSpringboot/model/entities/CorralEntity.java
@@ -1,9 +1,6 @@
 package RIwi.BackendSpringboot.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +13,8 @@ public class CorralEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+
     private Integer capacity;
 }
